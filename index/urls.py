7fb7e659d5eb8ace7c090f783c914df9db249c36
iliyasis <webiliya1504@gmail.com>
2025-08-31 16:34:10 +0330

    Completing App Index Templates and Translating Them +Adding to URL and View

--- index/urls.py
+++ index/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from index import views
 
+app_name = "index"
+
 urlpatterns = [
     path("", views.home,name="home"),
+    path("news/", views.news, name="news"),
+    path("blog/", views.blog, name="blog"),
+    path("about/", views.about, name="about"),
+    path("contact_us/", views.contact_us, name="contact_us"),
 ]
--- index/views.py
+++ index/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render
 def home(request):
     return render(request,"index/school-home.html")
 
+def news(request):
+    return render(request,"index/news-grid.html")
+
+def blog(request):
+    return render(request,"index/blog-grid.html")
+
+def about(request):
+    return render(request,"index/about.html")
+
+def contact_us(request):
+    return render(request,"index/contact-us.html")
