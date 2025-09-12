f87dc047f2a329032e5b9ee539aa2b33f0dfd0f1
iliyasis <webiliya1504@gmail.com>
2025-09-12 23:25:07 +0330

    complete blog app

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = "blog"
 urlpatterns = [
     path("", views.blog_list,name="blog_list"),
     path("detail/<slug>", views.blog_detail,name="blog_detail"),
+    path("manage/", views.blog_manage,name="blog_manage"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,8 @@ def blog_detail(request,slug):
     post = BlogPost.objects.get(slug=slug)
     context = {'post': post}
     return render(request, "blog/blog_detail.html", context)
+
+def blog_manage(request):
+    posts = BlogPost.objects.all()
+    context = {'posts': posts}
+    return render(request, "blog/manage/admin-course-list.html",context)
