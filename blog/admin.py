16d45ada8dead546d1a6753346ce6ea9f838ed08
iliyasis <webiliya1504@gmail.com>
2025-09-12 12:58:51 +0330

    create blog model and migrate that

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from blog.models import *
 # Register your models here.
+class BlogCategoryAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(BlogCategory, BlogCategoryAdmin)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status','created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(BlogPost, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish"),
+    (2,"Archived"),
+)
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    lead = models.CharField(max_length=600)
+    category = models.ManyToManyField(BlogCategory)
+    author = models.ForeignKey(User, on_delete= models.SET_NULL,related_name='blog_posts',null=True)
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now= True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
