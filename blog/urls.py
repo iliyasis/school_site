1a0866667856e156ad24aec37608bc7cac208c67
iliyasis <webiliya1504@gmail.com>
2025-09-22 03:55:40 +0330

    Organize sorting mode by categories and author

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ app_name = "blog"
 
 urlpatterns = [
     path("", views.blog_list,name="blog_list"),
+    path("category/<cat>", views.blog_list,name="category"),
+    path("author/<author>", views.blog_list,name="author"),
     path("detail/<slug>", views.blog_detail,name="blog_detail"),
     path("manage/", views.blog_manage,name="blog_manage"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from blog.models import *
 # Create your views here.
 
-def blog_list(request):
-    posts = BlogPost.objects.all()
-    context = {'posts': posts}
+def blog_list(request,author=None,cat=None):
+    if author:
+        posts = BlogPost.objects.filter(status=1,author__username=author)
+        context = {'posts': posts, 'author': author}
+    elif cat:
+        posts = BlogPost.objects.filter(status=1,category__name=cat)
+        context = {'posts': posts, 'cat': cat}
+    else:
+        posts = BlogPost.objects.filter(status=1)
+        context = {'posts': posts, }
+
+
     return render(request, "blog/blog_list.html", context)
 
 def blog_detail(request,slug):
