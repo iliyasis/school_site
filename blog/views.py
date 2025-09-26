d0e69170e11b463e395aed2819c4e1b529e4064f
iliyasis <webiliya1504@gmail.com>
2025-09-26 23:30:29 +0330

    edit search box into blog list

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,35 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 from blog.models import *
 # Create your views here.
 
-def blog_list(request,author=None,cat=None):
-    if author:
+def blog_list(request,author="None",cat="None"):
+    if author != "None":
         posts = BlogPost.objects.filter(status=1,author__username=author)
-        context = {'posts': posts, 'author': author}
-    elif cat:
+        #context = {'posts': posts, 'author': author}
+    elif cat != "None":
         posts = BlogPost.objects.filter(status=1,category__name=cat)
-        context = {'posts': posts, 'cat': cat}
-    elif request.method == 'GET' and request.GET.get('s'):
-        s = request.GET.get('s')
-        posts = BlogPost.objects.filter(title__contains=s)
-        context = {'posts': posts}
+       # context = {'posts': posts, 'cat': cat}
     else:
         posts = BlogPost.objects.filter(status=1)
-        context = {'posts': posts, }
+        #context = {'posts': posts, }
+
+    if request.method == 'GET' and request.GET.get('s'):
+        s = request.GET.get('s')
+        posts = posts.filter(title__contains=s)
+
+    posts = Paginator(posts, 3)
+    page_number = request.GET.get("page")
+
+    try:
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)#کار نمیکنه
+    except EmptyPage:
+        posts = posts.get_page(1)#کار نمیکنه
+
+
+    context = {'posts': posts, 'cat': cat, 'author': author}
 
 
     return render(request, "blog/blog_list.html", context)
@@ -28,4 +42,4 @@ def blog_detail(request,slug):
 def blog_manage(request):
     posts = BlogPost.objects.all()
     context = {'posts': posts}
-    return render(request, "blog/manage/admin-course-list.html",context)
+    return render(request, "blog/manage/admin-course-list.html",context)/
