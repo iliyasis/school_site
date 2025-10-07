892c054a12f18507ce5e9f9e1a7841c2a97199d6
iliyasis <webiliya1504@gmail.com>
2025-10-07 23:48:07 +0330

    add a new admin template

--- school_site/settings.py
+++ school_site/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "multi_captcha_admin",
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
